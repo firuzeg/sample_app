817701f7e38e422da987379d51d8c4f7997c697c
firuz <firuz.bsn@gmail.com>
2014-06-07 11:02:50 +0400

    Finish contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,4 +40,17 @@ describe "Static pages" do
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contactc'" do
+      visit '/static_pages/contacts'
+      expect(page).to have_content('contact page')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contacts'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
